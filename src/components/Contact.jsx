7f0519c98adff07cca94902ea79c36c41474db0e
Daniel Schofer <dschof41@gmail.com>
2021-05-03 21:06:31 -0400

    start contact form
    ctct frm

---
+++ src/components/Contact.jsx
@@ -0,0 +1,94 @@
+import React, {useState, useRef} from 'react'
+
+const Newsletter = () => {
+    const [formMessage, setFormMessage] = useState('hidden');
+    const contactForm = useRef(null)
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        let formData = new FormData(contactForm)
+        fetch('/', {
+          method: 'POST',
+          headers: { "Content-Type": "application/x-www-form-urlencoded" },
+          body: new URLSearchParams(formData).toString()
+        }).then(() => {
+            setFormMessage('success')
+            contactForm.reset()
+            console.log('Form successfully submitted')
+        }).catch((error) => {
+            setFormMessage('error')
+            console.log('Form submission error')
+            alert(error)
+            }
+        )
+    }
+
+  return (
+    <div className="container py-12 lg:py-16">
+      <h2 className="text-3xl sm:text-4xl font-extrabold text-gray-900 leading-tight tracking-tight">
+        Contact Me
+      </h2>
+      <form name="contact"
+            id="contact"
+            method="POST"
+            data-netlify="true"
+            data-netlify-honeypot="bot-field"
+            onSubmit={handleSubmit}
+            ref={contactForm}
+            class="w-full max-w-lg">
+        {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+        <input type="hidden" name="form-name" value="contact" />
+        <div hidden>
+            <label>
+            Don’t fill this out:{' '}
+            <input name="bot-field" />
+            </label>
+        </div>
+        <div class="flex flex-wrap -mx-3 mb-6">
+            <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
+                Name
+            </label>
+            <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Jane" required={true}/>
+            </div>
+            <div class="w-full md:w-1/2 px-3">
+            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
+                Last Name
+            </label>
+            <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Doe" required={true}/>
+            </div>
+        </div>
+        <div class="flex flex-wrap -mx-3 mb-6">
+            <div class="w-full px-3">
+            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
+                E-mail
+            </label>
+            <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="email" type="email" required={true}/>
+            <p class="text-gray-600 text-xs italic">Some tips - as long as needed</p>
+            </div>
+        </div>
+        <div class="flex flex-wrap -mx-3 mb-6">
+            <div class="w-full px-3">
+            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
+                Message
+            </label>
+            <textarea class=" no-resize appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none" id="message" required={true}></textarea>
+            </div>
+        </div>
+        <div class="md:flex md:items-center">
+            <div class="md:w-1/3">
+            <button class="shadow bg-teal-400 hover:bg-teal-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="submit">
+                Send
+            </button>
+            </div>
+            <div class="md:w-2/3"></div>
+        </div>
+        </form>
+        {formMessage === 'success' && (
+            <p>{formMessage}</p>
+        )}
+    </div>
+  )
+}
+
+export default Newsletter
