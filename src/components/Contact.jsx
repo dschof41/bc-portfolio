6257eba160dacc095201da3600877c36f274e298
dschof41 <dschof41@gmail.com>
2021-05-05 20:22:41 -0400

    update contacty
    Continue working on contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,24 +1,23 @@
 import React, {useState, useRef} from 'react'
 
-const Newsletter = () => {
+const Contact = () => {
     const [formMessage, setFormMessage] = useState('hidden');
     const contactForm = useRef(null)
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        let formData = new FormData(contactForm)
+        let formData = new FormData(contactForm.current)
         fetch('/', {
           method: 'POST',
           headers: { "Content-Type": "application/x-www-form-urlencoded" },
           body: new URLSearchParams(formData).toString()
         }).then(() => {
             setFormMessage('success')
-            contactForm.reset()
+            contactForm.current.reset()
             console.log('Form successfully submitted')
         }).catch((error) => {
             setFormMessage('error')
             console.log('Form submission error')
-            alert(error)
             }
         )
     }
@@ -35,60 +34,69 @@ const Newsletter = () => {
             data-netlify-honeypot="bot-field"
             onSubmit={handleSubmit}
             ref={contactForm}
-            class="w-full max-w-lg">
+            className="w-full max-w-lg">
         {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
-        <input type="hidden" name="form-name" value="contact" />
-        <div hidden>
-            <label>
-            Don’t fill this out:{' '}
-            <input name="bot-field" />
-            </label>
-        </div>
-        <div class="flex flex-wrap -mx-3 mb-6">
-            <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
-                Name
-            </label>
-            <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Jane" required={true}/>
+            <input type="hidden" name="form-name" value="contact" />
+            <div hidden>
+                <label>
+                Don’t fill this out:{' '}
+                <input name="bot-field" />
+                </label>
             </div>
-            <div class="w-full md:w-1/2 px-3">
-            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
-                Last Name
-            </label>
-            <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Doe" required={true}/>
+            <div className="flex flex-wrap -mx-3 mb-6">
+                <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-first-name">
+                    Name
+                </label>
+                <input className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Jane" required={true}/>
+                </div>
+                <div className="w-full md:w-1/2 px-3">
+                <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-last-name">
+                    Last Name
+                </label>
+                <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Doe" required={true}/>
+                </div>
             </div>
-        </div>
-        <div class="flex flex-wrap -mx-3 mb-6">
-            <div class="w-full px-3">
-            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
-                E-mail
-            </label>
-            <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="email" type="email" required={true}/>
-            <p class="text-gray-600 text-xs italic">Some tips - as long as needed</p>
+            <div className="flex flex-wrap -mx-3 mb-6">
+                <div className="w-full px-3">
+                <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-password">
+                    E-mail
+                </label>
+                <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="email" type="email" required={true}/>
+                <p className="text-gray-600 text-xs italic">Enter the best email to reach you</p>
+                </div>
             </div>
-        </div>
-        <div class="flex flex-wrap -mx-3 mb-6">
-            <div class="w-full px-3">
-            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
-                Message
-            </label>
-            <textarea class=" no-resize appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none" id="message" required={true}></textarea>
+            <div className="flex flex-wrap -mx-3 mb-6">
+                <div className="w-full px-3">
+                <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-password">
+                    Message
+                </label>
+                <textarea className=" no-resize appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none" id="message" required={true}></textarea>
+                </div>
             </div>
-        </div>
-        <div class="md:flex md:items-center">
-            <div class="md:w-1/3">
-            <button class="shadow bg-teal-400 hover:bg-teal-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="submit">
-                Send
-            </button>
+            <div className="md:flex md:items-center">
+                <div className="md:w-1/3">
+                    <button className="shadow bg-blue-600 hover:bg-teal-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="submit">
+                        Send
+                    </button>
+                </div>
+                <div className="md:w-2/3"></div>
             </div>
-            <div class="md:w-2/3"></div>
-        </div>
         </form>
         {formMessage === 'success' && (
-            <p>{formMessage}</p>
+            <div class="flex items-center bg-green-500 text-white text-sm font-bold px-4 py-3" role="alert">
+                <svg class="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M12.432 0c1.34 0 2.01.912 2.01 1.957 0 1.305-1.164 2.512-2.679 2.512-1.269 0-2.009-.75-1.974-1.99C9.789 1.436 10.67 0 12.432 0zM8.309 20c-1.058 0-1.833-.652-1.093-3.524l1.214-5.092c.211-.814.246-1.141 0-1.141-.317 0-1.689.562-2.502 1.117l-.528-.88c2.572-2.186 5.531-3.467 6.801-3.467 1.057 0 1.233 1.273.705 3.23l-1.391 5.352c-.246.945-.141 1.271.106 1.271.317 0 1.357-.392 2.379-1.207l.6.814C12.098 19.02 9.365 20 8.309 20z"/></svg>
+                <p>Thanks! Message received!</p>
+            </div>
+        )}
+        {formMessage === 'error' && (
+            <div class="flex items-center bg-red-500 text-white text-sm font-bold px-4 py-3" role="alert">
+                <svg class="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M12.432 0c1.34 0 2.01.912 2.01 1.957 0 1.305-1.164 2.512-2.679 2.512-1.269 0-2.009-.75-1.974-1.99C9.789 1.436 10.67 0 12.432 0zM8.309 20c-1.058 0-1.833-.652-1.093-3.524l1.214-5.092c.211-.814.246-1.141 0-1.141-.317 0-1.689.562-2.502 1.117l-.528-.88c2.572-2.186 5.531-3.467 6.801-3.467 1.057 0 1.233 1.273.705 3.23l-1.391 5.352c-.246.945-.141 1.271.106 1.271.317 0 1.357-.392 2.379-1.207l.6.814C12.098 19.02 9.365 20 8.309 20z"/></svg>
+                <p>Sorry, we're unable to accept your submission at this time, please try again later.</p>
+            </div>
         )}
     </div>
   )
 }
 
-export default Newsletter
+export default Contact
